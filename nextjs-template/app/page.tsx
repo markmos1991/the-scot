b725d87b088258d2a974d2e653ad680e45a035ba
Mark <101826190+markmos1991@users.noreply.github.com>
2024-04-27 13:01:22 +0100

    Initial commit

---
+++ nextjs-template/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function page() {
+  return (
+    <section className="py-10">
+      <div className="container">
+        <h1 className="text-3xl">DEMO</h1>
+      </div>
+    </section>
+  )
+}
+
+export default page
